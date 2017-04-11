4daca25ec9435984249599553819f1f24ebe0f58
Matt Streit <mwstreit@gmail.com>
2017-04-11 13:39:46 -0500

    adds caesar cipher to repo

---
+++ caesar_cipher.rb
@@ -0,0 +1,28 @@
+#cipher takes a string, converts each character to a number, adds the specified
+#offset to the characters, and converts the numbers back into a string
+
+
+def caesar_cipher(string, offset)
+  coded = ''
+  string.scan(/./) do |char|
+    if ("a".."z").include? (char.downcase)
+      offset.times {char = char.next}
+      new_char = char[-1]
+    elsif ("A".."Z").include? (char.upcase)
+      offset.times {char = char.next}
+      new_char = char[-1]
+    else
+      new_char = char
+    end
+    coded << new_char
+  end
+  return coded
+end
+
+puts 'What would you like to encode?'
+string = gets.chomp
+puts 'What would you like the offset to be?'
+offset = gets.chomp
+offset = offset.to_i
+
+puts caesar_cipher(string, offset)
